e4c5bddf7c68486259d483a158e1a6c7b414da96
aju <ajinkyabmadane03@gmail.con>
2025-06-07 22:46:10 +0530

    AI blog generator made and at this point google gemini also integrated

---
+++ server/models/Blog.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const blogSchema= new mongoose.Schema({
+    title:{type:String, required:true},
+    subtitle:{type:String},
+    description:{type:String, required:true},
+    category:{type:String, required:true},
+    image:{type:String, required:true},
+    isPublished:{type:Boolean, required:true}
+},{timestamps:true});
+
+const Blog = mongoose.model('blog', blogSchema);
+
+export default Blog;
